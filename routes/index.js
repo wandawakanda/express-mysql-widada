522c8944bd4f1f4b96efcf42c088ec0845286568
wandawakanda <wandadoremi@gmail.com>
2025-01-15 12:17:02 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/json", function (req, res, next) {
+  res.json({ message: "Hello, world!" });
+});
+
+module.exports = router;
