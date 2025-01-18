e5af88bdc59809bb38d4e167e652711cd41bffbe
wandawakanda <wandadoremi@gmail.com>
2025-01-18 11:08:07 +0700

    modify routes products.js to add a record

--- routes/products.js
+++ routes/products.js
@@ -1,18 +1,35 @@
 var express = require("express");
 var router = express.Router();
+const Validator = require("fastest-validator");
+const v = new Validator();
+
+const { Product } = require("../models");
+
 router.post("/", async (req, res, next) => {
-  //   try {
-  //     const { name, brand, description } = req.body;
-  //     const product = await Product.create({
-  //       name,
-  //       brand,
-  //       description,
-  //     });
-  //     res.send(product);
-  //   } catch (err) {
-  //     next(err);
-  //   }
-  res.send("Hello world");
+  const schema = {
+    name: "string",
+    brand: "string",
+    description: "string|optional",
+  };
+  const validate = v.validate(req.body, schema);
+
+  if (validate.length) {
+    //ada error
+    return res.status(400).json({
+      status: "error",
+      message: validate,
+    });
+  }
+  //res.send("ok");
+  const { name, brand, description } = req.body;
+  const product = await Product.create({
+    name,
+    brand,
+    description,
+  });
+  //res.send(product);
+  //return product;
+  return res.json(product);
 });
 
 module.exports = router;
