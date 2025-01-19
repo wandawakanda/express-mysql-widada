ca78dc5d9a1da6b1afa6b2dd6c237fb469832e97
wandawakanda <wandadoremi@gmail.com>
2025-01-19 14:18:13 +0700

    add router.put that handle edit/modify product record

--- routes/products.js
+++ routes/products.js
@@ -33,4 +33,37 @@ router.post("/", async (req, res, next) => {
   return res.json(product);
 });
 
+router.put("/:id", async (req, res) => {
+  //update a record of product
+  const id = req.params.id;
+
+  //res.send(id);
+  let product = await Product.findByPk(id);
+  if (!product) {
+    return res.status(404).json({
+      status: "error",
+      message: "Product not found",
+    });
+  }
+
+  const schema = {
+    name: "string|optional",
+    brand: "string|optional",
+    description: "string|optional",
+  };
+  const validate = v.validate(req.body, schema);
+
+  if (validate.length) {
+    //ada error
+    return res.status(400).json({
+      status: "error",
+      message: validate,
+    });
+  }
+
+  product = await product.update(req.body);
+  return res.json(product);
+  //res.send("ok");
+});
+
 module.exports = router;
